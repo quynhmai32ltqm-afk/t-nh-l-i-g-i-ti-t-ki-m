8e167f89b406e52ed01bd63cb83af7631a7e3240
quynhmai32ltqm-afk <quynhmai32.ltqm@gmail.com>
2026-06-16 16:43:56 +0700

    Update app title and add logo image

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
-
+st.image("logo.jpg")
 # Tiêu đề ứng dụng
-st.title("💰 Ứng dụng tính tiền gửi tiết kiệm")
+st.title("APP TÍNH TIỀN GỬI TIẾT KIỆM_ĐỀ TÀI 7_LÊ THỊ QUỲNH MAI")
 
 # Nhập dữ liệu
 C = st.number_input(
